5518fe3ec6cdc9380dd9bf5c84439c2db0e6f6ae
MCrux43 <misael.ortega25@gmail.com>
2022-09-13 01:48:37 -0500

    no carga css

--- MVT_MisaelOrtega/view.py
+++ MVT_MisaelOrtega/view.py
@@ -5,7 +5,7 @@ from django.template import loader
 
 def homie(self):
     data = {'name': 'Julia', 'age': '28', 'zodiac':'piscis'}
-    plantilla = loader.get_template('static/index.html')
+    plantilla = loader.get_template('index.html')
     documento = plantilla.render(data)
     return HttpResponse (documento)
 
