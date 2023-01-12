e022feaf6881fea046c317de989838e1c2bf2125
MCrux43 <misael.ortega25@gmail.com>
2023-01-11 23:05:32 -0600

    hola de nuevo

--- MVT_MisaelOrtega/urls.py
+++ MVT_MisaelOrtega/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', inicio),
+    path('/', inicio),
     path('suenios/', include('suenios.urls'))
 ]
 
